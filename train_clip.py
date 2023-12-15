9db96d0c604688a9920745736aa4cab680293706
Tobias Hallingstad <tobias@hallingstad.me>
2023-12-15 14:02:26 +0100

    refactor of finetuning clip code

--- train_clip.py
+++ train_clip.py
@@ -22,104 +22,25 @@ from modules.utils import set_phos_version, set_phoc_version, gen_shape_descript
 
 from modules import models, residualmodels
 
+# Load the CLIP model
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+
 # Initialize logging
 def setup_logging(log_file_path):
     logging.basicConfig(filename=log_file_path, level=logging.INFO, 
                         format='%(asctime)s - %(levelname)s - %(message)s', 
                         datefmt='%Y-%m-%d %H:%M:%S')
+    
     logging.info("Initialized logging")
 
-model_save_path = ospj('models', 'fine-tuned_clip', 'model.pth')
-log_file_path = ospj('models', 'fine-tuned_clip', 'training_log.log')
-setup_logging(log_file_path)
 
-# Load the CLIP model
-device = "cuda" if torch.cuda.is_available() else "cpu"
-# device = 'cpu'
-
-clip_model, clip_preprocess = clip.load("ViT-B/32", device=device)
-
-clip_model.float()
-
-model_save_path = ospj('models', 'fine-tuned_clip', 'model.pth')
-
-# Define phosc model
-phosc_model = create_model(
-    model_name='ResNet18Phosc',
-    phos_size=195,
-    phoc_size=1200,
-    phos_layers=1,
-    phoc_layers=1,
-    dropout=0.5
-).to(device)
-
-# Sett phos and phoc language
-set_phos_version('ben')
-set_phoc_version('ben')  
-
-# Get dataset
-trainset = dset.CompositionDataset(
-    root=ospj(DATA_FOLDER, "BengaliWords", "BengaliWords_CroppedVersion_Folds"),
-    phase='train',
-    split='Fold0_use',
-    model='resnet18',
-    num_negs=1,
-    pair_dropout=0.5,
-    update_features = False,
-    train_only=True,
-    open_world=True,
-    augmented=True,
-    phosc_model=phosc_model,
-)
-
-train_loader = torch.utils.data.DataLoader(
-    trainset,
-    batch_size=64,
-    shuffle=True,
-    num_workers=0
-)
-
-validationset = dset.CompositionDataset(
-    root=ospj(DATA_FOLDER, "BengaliWords", "BengaliWords_CroppedVersion_Folds"),
-    phase='val',
-    split='Fold0_use',
-    model='resnet18',
-    num_negs=1,
-    pair_dropout=0.5,
-    update_features = False,
-    train_only=True,    # TODO: change to `False`
-    open_world=True,
-    augmented=True,
-    phosc_model=phosc_model,
-)
-
-val_loader = torch.utils.data.DataLoader(
-    trainset,
-    batch_size=64,
-    shuffle=True,
-    num_workers=0
-)
-
-# Early stopping parameters
-patience = 5
-best_loss = float('inf')
-counter = 0
-
-
-def custom_loss(image_features, text_features):
-    # Assuming image_features and text_features are normalized
-    similarity = torch.nn.functional.cosine_similarity(text_features, image_features)
-    loss = torch.mean(1 - similarity)  # Penalize high similarity
-    return loss
-
-
-def gen_word_objs_embeddings(obj):
+def gen_word_objs_embeddings(obj, model):
     shape_description = gen_shape_description(obj)
     text = clip.tokenize(shape_description).to(device)
 
     with torch.no_grad():
-        text_features = clip_model.encode_text(text)
-
+        text_features = model.encode_text(text)
 
     return text_features
 
@@ -139,18 +60,6 @@ def custom_loss(image_features, text_features):
     return loss
 
 
-def custom_triplet_loss(anchor_image_features, positive_text_features, negative_text_features):
-    # Use torch.nn.functional.triplet_margin_loss
-    loss = torch.nn.functional.triplet_margin_loss(anchor_image_features, positive_text_features, negative_text_features, margin=1.0)
-    return loss
-
-
-def has_nan(tensor):
-    if torch.isnan(tensor).any():
-        print("NaN value found in tensor. Exiting...")
-        exit()
-
-
 def custom_loss_same_class(anchor_image_features, positive_text_features, negative_text_features):
     # Assuming anchor_image_features and positive_text_features are normalized
     similarity = torch.nn.functional.cosine_similarity(anchor_image_features, positive_text_features)
@@ -167,54 +76,105 @@ def custom_loss_different_class(anchor_image_features, positive_text_features, n
     return loss
 
 
-def main(): 
-    global patience, best_loss, counter
+def convert_models_to_fp32(model): 
+    """
+    https://github.com/openai/CLIP/issues/57
+    """
+    for p in model.parameters(): 
+        p.data = p.data.float() 
+        p.grad.data = p.grad.data.float()
 
-    # Fine-tuning (simple example)
-    optimizer = torch.optim.Adam(clip_model.parameters(), lr=1e-5)
+
+def train_one_epoch(epoch, train_loader, clip_model, clip_preprocess, loader, optimizer):
+    global device
+
     clip_model.train()
+    running_loss = 0
 
-    loader = ImageLoader(ospj(DATA_FOLDER, "BengaliWords", "BengaliWords_CroppedVersion_Folds", 'Fold0_use'))
+    train_bar = tqdm(train_loader, desc=f"Training Epoch {epoch + 1}")
+    for batch in train_bar:
+        accumulated_loss = 0
 
-    # Fine-tuning (with early stopping)
-    optimizer = torch.optim.Adam(clip_model.parameters(), lr=1e-5)
-    clip_model.train()
+        optimizer.zero_grad()
 
-    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', patience=2, factor=0.5, verbose=True)
+        # Unpacking the batch data
+        _, _, _, _, _, _, _, _, image_names, _, descriptions = batch
 
-    model_save_path = ospj('models', 'fine-tuned_clip', 'model.pth')
+        cash_description = [gen_word_objs_embeddings(description, clip_model) for description in descriptions]
 
-    verify_model_save_path(model_save_path)
+        for i in range(len(image_names)):
+            anchor_img_name = image_names[i]
 
-    for epoch in range(100):  # Set a maximum number of epochs
-        clip_model.train()
-        running_loss = 0.0
+            # Load and preprocess the anchor image
+            anchor_img = loader(anchor_img_name)
+            anchor_img = clip_preprocess(anchor_img).unsqueeze(0).to(device)
 
-        train_bar = tqdm(train_loader, desc=f"Training Epoch {epoch + 1}")
-        for batch in train_bar:
-            accumulated_loss = torch.zeros(1, device=device, requires_grad=True)
+            anchor_image_features = clip_model.encode_image(anchor_img)
+            anchor_text_features = cash_description[i]
+
+            for j in range(len(image_names)):
+                if i != j:
+                    negative_text_features = cash_description[j] 
+
+                    # Determine if descriptions are of the same class
+                    is_same_class = (descriptions[i] == descriptions[j])
+
+                    # Calculate custom loss based on class
+                    if is_same_class:
+                        # Minimize distance for same class
+                        loss = custom_loss_same_class(anchor_image_features, anchor_text_features, negative_text_features)
+                    else:
+                        # Maximize distance for different classes
+                        loss = custom_loss_different_class(anchor_image_features, anchor_text_features, negative_text_features)
+
+                    accumulated_loss += loss  # Accumulate loss
+            
+        # Normalize loss by the number of comparisons
+        normalized_loss = accumulated_loss / (len(image_names) * (len(image_names) - 1))
+
+        normalized_loss.backward()  # Backpropagate the normalized loss
+        optimizer.step()
+        running_loss += normalized_loss.item()
+
+        train_bar.set_description(f"Training Epoch {epoch + 1} Loss: {normalized_loss.item():.4f}")
+
+    # Logging training loss
+    train_loss = running_loss / len(train_loader)
+    logging.info(f"Epoch {epoch}, Training Loss: {train_loss}")
+
+    return train_loss
+
+
+def validate_one_epoch(epoch, val_loader, clip_model, clip_preprocess, loader, scheduler):
+    clip_model.eval()
+    val_loss = 0.0
+
+    with torch.no_grad():
+        val_bar = tqdm(val_loader, desc=f"Validation Epoch {epoch + 1}")
+        for batch in val_bar:
+            accumulated_loss = torch.zeros(1, device=device)
 
             # Unpacking the batch data
             _, _, _, _, _, _, _, _, image_names, _, descriptions = batch
 
-            cash_description = [gen_word_objs_embeddings(description) for description in descriptions]
-
             for i in range(len(image_names)):
                 anchor_img_name = image_names[i]
+                anchor_desc = descriptions[i]
 
                 # Load and preprocess the anchor image
                 anchor_img = loader(anchor_img_name)
                 anchor_img = clip_preprocess(anchor_img).unsqueeze(0).to(device)
 
                 anchor_image_features = clip_model.encode_image(anchor_img)
-                anchor_text_features = cash_description[i]
+                anchor_text_features = gen_word_objs_embeddings(anchor_desc, clip_model)
 
                 for j in range(len(image_names)):
                     if i != j:
-                        negative_text_features = cash_description[j] 
+                        negative_desc = descriptions[j]
+                        negative_text_features = gen_word_objs_embeddings(negative_desc, clip_model)
 
                         # Determine if descriptions are of the same class
-                        is_same_class = (descriptions[i] == descriptions[j])
+                        is_same_class = (anchor_desc == negative_desc)
 
                         # Calculate custom loss based on class
                         if is_same_class:
@@ -224,98 +184,152 @@ def main():
                             # Maximize distance for different classes
                             loss = custom_loss_different_class(anchor_image_features, anchor_text_features, negative_text_features)
 
-                        accumulated_loss = accumulated_loss + loss  # Out-of-place operation
-            
+                        accumulated_loss += loss
+
             # Normalize loss by the number of comparisons
-            normalized_loss = accumulated_loss / (len(image_names) * (len(image_names) - 1))
+            normalized_loss = accumulated_loss.mean()
+            val_loss += normalized_loss.item()
+            val_bar.set_description(f"Validation Epoch {epoch + 1} Loss: {normalized_loss.item():.4f}")
 
-            optimizer.zero_grad()
-            normalized_loss.backward()  # Backpropagate the normalized loss
-            optimizer.step()
-            running_loss += normalized_loss.item()
+        val_loss /= len(val_loader)
 
-            train_bar.set_description(f"Training Epoch {epoch + 1} Loss: {normalized_loss.item():.4f}")
+    # Logging validation loss
+    logging.info(f"Epoch {epoch}, Validation Loss: {val_loss}")
 
-        # Logging training loss
-        train_loss = running_loss / len(train_loader)
-        logging.info(f"Epoch {epoch}, Training Loss: {train_loss}")
+    scheduler.step(val_loss)
 
-        # Validation step
-        clip_model.eval()
-        with torch.no_grad():
-            val_loss = 0.0
+    return val_loss
 
-            val_bar = tqdm(val_loader, desc=f"Validation Epoch {epoch + 1}")
-            for batch in val_bar:
-                accumulated_loss = torch.zeros(1, device=device)
 
-                # Unpacking the batch data
-                _, _, _, _, _, _, _, _, image_names, _, descriptions = batch
-                
-                cash_description = [gen_word_objs_embeddings(description) for description in descriptions]
+def main(): 
+    global clip_model, clip_preprocess, model_save_path
+    # Datasett variables
+    split = 'Fold0_use_50'
+    use_augmented = False
 
-                for i in range(len(image_names)):
-                    anchor_img_name = image_names[i]
-                    anchor_desc = descriptions[i]
+    # Loader variables
+    batch_size = 64
 
-                    # Load and preprocess the anchor image
-                    anchor_img = loader(anchor_img_name)
-                    anchor_img = clip_preprocess(anchor_img).unsqueeze(0).to(device)
+    # Define patchs
+    log_file_path = ospj('models', 'fine-tuned_clip', split, 'training_log.log')
+    root_dir = ospj(DATA_FOLDER, "BengaliWords", "BengaliWords_CroppedVersion_Folds")
+    model_save_path = ospj('models', 'fine-tuned_clip', split, 'model.pth')
+    image_loader_path = ospj(root_dir, split)
 
-                    anchor_image_features = clip_model.encode_image(anchor_img)
-                    anchor_text_features = cash_description[i]
+    verify_model_save_path(model_save_path)
 
-                    for j in range(len(image_names)):
-                        if i != j:
-                            negative_text_features = cash_description[j] 
+    setup_logging(log_file_path)
 
-                            # Determine if descriptions are of the same class
-                            is_same_class = (descriptions[i] == descriptions[j])
+    # Set up models
+    clip_model, clip_preprocess = clip.load("ViT-B/32", device=device)
+    clip_model.float()
 
-                            # Calculate custom loss based on class
-                            if is_same_class:
-                                # Minimize distance for same class
-                                loss = custom_loss_same_class(anchor_image_features, anchor_text_features, negative_text_features)
-                            else:
-                                # Maximize distance for different classes
-                                loss = custom_loss_different_class(anchor_image_features, anchor_text_features, negative_text_features)
+    phosc_model = create_model(
+        model_name='ResNet18Phosc',
+        phos_size=195,
+        phoc_size=1200,
+        phos_layers=1,
+        phoc_layers=1,
+        dropout=0.5
+    ).to(device)
 
-                            accumulated_loss += loss
+    # Sett phos and phoc language
+    set_phos_version('ben')
+    set_phoc_version('ben')  
 
-                # Normalize loss by the number of comparisons
-                normalized_loss = accumulated_loss.mean()
-                val_loss += normalized_loss.item()
-                val_bar.set_description(f"Validation Epoch {epoch + 1} Loss: {normalized_loss.item():.4f}")
+    # Setup datasett
+    trainset = dset.CompositionDataset(
+        root=root_dir,
+        phase='train',
+        split=split,
+        model='resnet18',
+        num_negs=1,
+        pair_dropout=0.5,
+        update_features = False,
+        train_only=True,
+        open_world=True,
+        augmented=use_augmented,
+        phosc_model=phosc_model,
+    )
 
-            val_loss /= len(val_loader)
-            # Logging validation loss
-            logging.info(f"Epoch {epoch}, Validation Loss: {val_loss}")
+    validationset = dset.CompositionDataset(
+        root=root_dir,
+        phase='val',
+        split=split,
+        model='resnet18',
+        num_negs=1,
+        pair_dropout=0.5,
+        update_features = False,
+        train_only=False,
+        open_world=True,
+        augmented=use_augmented,
+        phosc_model=phosc_model,
+    )
 
-            scheduler.step(val_loss)
 
-            print(f"Epoch {epoch}, Training Loss: {running_loss / len(train_loader)}, Validation Loss: {val_loss}, Patience: {counter}")
-            logging.info(f"Epoch {epoch}, Best Loss: {best_loss}, Patience Counter: {counter}")
+    train_loader = torch.utils.data.DataLoader(
+        trainset,
+        batch_size=batch_size,
+        shuffle=True,
+        num_workers=0
+    )
 
-            # Check for early stopping
-            if val_loss < best_loss:
-                best_loss = val_loss
-                counter = 0
-                # Save the model
-                torch.save(clip_model.state_dict(), model_save_path)
-            else:
-                counter += 1
-                if counter >= patience:
-                    print("Early stopping triggered")
-                    break
+    val_loader = torch.utils.data.DataLoader(
+        validationset,
+        batch_size=batch_size,
+        shuffle=True,
+        num_workers=0
+    )
+
+    # Define image loader
+    loader = ImageLoader(image_loader_path)
+
+    # Validation related variables
+    patience = 5
+    best_loss = float('inf')
+    patience_counter = 0
+
+    # Fine-tuning
+    optimizer = torch.optim.Adam(clip_model.parameters(), lr=5e-5, betas=(0.9,0.98), eps=1e-6, weight_decay=0.2)
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', patience=2, factor=0.5, verbose=True)
+
+    for epoch in range(100):  # Set a maximum number of epochs
+        train_loss = train_one_epoch(
+            epoch,
+            train_loader,
+            clip_model,
+            clip_preprocess,
+            loader,
+            optimizer
+        )
+
+        val_loss = validate_one_epoch(
+            epoch,
+            val_loader,
+            clip_model,
+            clip_preprocess,
+            loader,
+            scheduler
+        )
+
+        l = val_loss
+
+        if l < best_loss:
+            print(f'{epoch}: New best loss: {l}')
+            torch.save(clip_model.state_dict(), model_save_path)
+
+            best_loss = l
+            patience_counter = 0
+        else:
+            patience_counter += 1
+
+            if patience_counter >= patience:
+                print(f'Early exit, patience reached')
+                break
+
 
 if __name__ == '__main__':
     try:
         main()
     except KeyboardInterrupt:
-        print('Early exit')
-        print(f'Best loss: {best_loss}')
-
-        logging.info(f'Best loss: {best_loss}')
-        # Save the model
-        torch.save(clip_model.state_dict(), model_save_path)
-        print(f"Model saved to {model_save_path}")
+        print('Ctrl-C exit')
